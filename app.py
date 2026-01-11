a6f08888b70252f70a925e3158c28850a1f01d5e
BarryS27 <238927804+BarryS27@users.noreply.github.com>
2026-01-11 14:52:07 -0500

    Change work route to '/work.html'

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 def index():
     return render_template("index.html")
 
-@app.route("/work")
+@app.route('/work.html')
 def work():
     return render_template("work.html")
 
