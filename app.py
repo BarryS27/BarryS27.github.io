e53c0484202d07bc048f8cce9a5669cf2fb69e90
BarryS27 <238927804+BarryS27@users.noreply.github.com>
2026-01-15 22:15:51 +0000

    revise the sentences

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/work.html')
+@app.route('/work')
 def work():
     return render_template("work.html")
 
-@app.route('/404.html')
-def render_404_static():
-    return render_template('404.html')
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
