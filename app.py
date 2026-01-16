bdc423110fb01ea0ebc10f5fe319eec2dee74bec
BarryS27 <238927804+BarryS27@users.noreply.github.com>
2026-01-16 00:38:08 +0000

    now

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-from datetime import datetime
+from datetime import datetime, timezone
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.context_processor
 def inject_now():
-    return {'now': datetime.utcnow()}
+    return {'now': datetime.now(timezone.utc)}
 
 @app.route("/")
 def index():
