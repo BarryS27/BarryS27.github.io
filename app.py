8fc3fa74d7fa318efd505e40c1ea8954a40d1374
BarryS27 <238927804+BarryS27@users.noreply.github.com>
2026-01-19 19:21:35 +0000

    bug

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from datetime import datetime, timezone
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.context_processor
+def inject_now():
+    return {'now': datetime.now(timezone.utc)}
 
 @app.route("/")
 def index():
@@ -9,6 +14,14 @@ def index():
 @app.route('/work.html')
 def work():
     return render_template("work.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route('/404.html')
+def render_404():
+    return render_template('404.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
