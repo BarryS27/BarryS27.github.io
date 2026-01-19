e349a71e23cb9b3377f0d178be7095c5462012b7
BarryS27 <238927804+BarryS27@users.noreply.github.com>
2026-01-19 14:14:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-@app.context_processor
-def inject_now():
-    return {'now': datetime.now(timezone.utc)}
-
 @app.route("/")
 def index():
     return render_template("index.html")
